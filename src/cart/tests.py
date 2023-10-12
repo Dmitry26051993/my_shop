7ff1223035610c7f720bcaf0e3933782fe4e3e03
Dmitry26051993 <126193336+Dmitry26051993@users.noreply.github.com>
2023-10-13 00:48:41 +0300

    Update tests.py

--- src/cart/tests.py
+++ src/cart/tests.py
@@ -1,5 +1,10 @@
-from django.test import TestCase
+from cart.views import cart_add, cart_remove, cart_detail
 from my_shop.models import Category, Product
+from my_shop.views import product_list, product_detail
+from django.test import TestCase, RequestFactory
+from orders.models import OrderItem, Order
+from cart.cart import Cart
+from orders.views import order_create, admin_order_detail
 
 
 class CategoryModelTest(TestCase):
@@ -8,7 +13,7 @@ class CategoryModelTest(TestCase):
         Category.objects.create(name='Test Category', slug='test-category')
 
     def test_name_label(self):
-        category = Category.objects.get(id=1)
+        category = Category.objects.get()
         category.name = category._meta.get_field('name').verbose_name
         category.slug = category._meta.get_field('slug').verbose_name
         self.assertEquals(category.name, 'Название'), (category.slug, 'Сокращ_урл')
@@ -18,7 +23,7 @@ class ProductModelTest(TestCase):
     def setUpTestData(cls):
         Category.objects.create(name='Test Category', slug='test-category')
         category = Category.objects.get(id=1)
-        Product.objects.create(category=category, name='Test Product', slug='test-product', price=10.00, stock=100)
+        Product.objects.create(category=category, name='Test Product', slug='test-product', price=1, stock=1)
 
     def test_name_label(self):
         product = Product.objects.get(id=1)
@@ -31,4 +36,67 @@ class ViewsTestCase(TestCase):
         response = self.client.get('http://127.0.0.1:8000')
         self.assertEqual(response.status_code, 200)
 
+class CartTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.product = Product.objects.create(name='Test Product', price=10)
 
+    def test_cart_add(self):
+        request = self.factory.post('/cart/add/', {'quantity': 1, 'update': False})
+        request.session = {}
+        response = cart_add(request, product_id=self.product.id)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/cart/cart_detail/')
+
+    def test_cart_remove(self):
+        request = self.factory.post('/cart/remove/', {})
+        request.session = {}
+        Cart(request).add(product=self.product, quantity=1)
+        response = cart_remove(request, product_id=self.product.id)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/cart/cart_detail/')
+
+    def test_cart_detail(self):
+        request = self.factory.get('/cart/detail/')
+        request.session = {}
+        Cart(request).add(product=self.product, quantity=1)
+        response = cart_detail(request)
+        self.assertEqual(response.status_code, 200)
+
+class ProductTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.category = Category.objects.create(name='Test Category', slug='test-category')
+        self.product = Product.objects.create(name='Test Product', price=10, available=True, category=self.category)
+
+    def test_product_list(self):
+        request = self.factory.get('/product/list/')
+        response = product_list(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Test Category')
+        self.assertContains(response, 'Test Product')
+
+    def test_product_detail(self):
+        request = self.factory.get('/product/detail/')
+        response = product_detail(request, id=self.product.id, slug=self.product.slug)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Test Product')
+
+class OrderTestCase(TestCase):
+
+    def test_order_create(self):
+        request = self.factory.post('/order/create/', {'first_name': 'Test', 'last_name': 'Test', 'email': 'test@example.com', 'phone': '+3752222653', 'address': 'Test address',  'city': 'Test city', 'comment': 'test'})
+        request.session = {}
+        Cart(request).add(product=self.product, quantity=1)
+        response = order_create(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Test Product')
+
+    def test_admin_order_detail(self):
+        request = self.factory.get('/admin/order/detail/')
+        request.user = self.user
+        order = Order.objects.create(first_name='Test', last_name='Test', email='test@example.com', phone='+3752222653', address='Test address',  city='Test city', comment='test')
+        OrderItem.objects.create(order=order, product=self.product, price=10, quantity=1)
+        response = admin_order_detail(request, order_id=order.id)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Test Product')
